758dfc9f93216ce9eb476513abb5a1971738c26e
mossmaps <134527323+mossmaps@users.noreply.github.com>
2023-09-30 10:30:36 -0400

    Testing

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var map = L.map('map').setview([52.52, 13.405], 10);
+
 L.tileLayer('https://tile.openstreetmap.org/${z}/${x}/${y}.png'), {
     maxZoom: 19,
     attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 }.addTo(map);
+
+var marker = L.marker([52.52, 13.405]).addTo(map);
