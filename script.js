6d8215ebdb2cfb548c3e2fb78617de1473c23487
mossmaps <134527323+mossmaps@users.noreply.github.com>
2023-11-09 22:02:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 var map = L.map('map').setView([52.52, 13.405], 10);
 
-var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
-
-L.tileLayer(OpenStreeMap_Mapnik).addto(map);
+L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
