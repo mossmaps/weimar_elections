2159d9d1299f26998f3dec2571fc527b924a5847
mossmaps <134527323+mossmaps@users.noreply.github.com>
2023-11-09 22:25:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ var map = L.map('map').setView([52.52, 13.405], 10);
 
 L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
 
-L.geoJSON('Weimar Election Data.geojson').addTo(map);
+L.geoJSON('./Weimar Election Data.geojson').addTo(map);
