97aa9ed3a2c4106c0387fa80857272a4c52a89dc
mossmaps <134527323+mossmaps@users.noreply.github.com>
2023-11-12 19:04:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -64,6 +64,6 @@ var defaultStyle = {
 L.geoJSON(electionResults, {
     style: defaultStyle,
     onEachFeature: function (feature, layer){
-        layer.bindPopup(`District: ${engName}`) 
+        layer.bindPopup(engName) 
     }
 }).addTo(map)
